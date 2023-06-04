d87e7db6ab5f7f2821318fa28b3c9f6dce70c668
Ashamango <ashaanupdas@gmail.com>
2023-06-03 21:12:45 -0400

    Outputs a 5% sales tax and displays the price and the computed sales tax

---
+++ src/SalesTax.java
@@ -0,0 +1,11 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class SalesTax {
+    public static void main(String[] args) {
+        // Declare variables
+        double purchase = 20.50;
+        double salesTax = 0;
+        salesTax = purchase * 0.05;
+        System.out.println("The price is " + purchase + " dollars and the computed sales tax is " + salesTax + " dollars");
+    }
+}
